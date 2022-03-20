8ccb7bed26086a8868f555f415dedb0aef455cfd
Anton Honcharov <antonhoncharovv@gmail.com>
2022-03-20 17:35:07 +0300

    makes first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyColor = document.querySelector("body");
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let startBtnIsActive = false;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+startBtn.addEventListener("click", () => {
+    if (startBtnIsActive) {
+        return;
+    }
+    timerId = setInterval(() => {
+        bodyColor.style.backgroundColor = getRandomHexColor();    
+    }, 1000);
+    startBtnIsActive = true;
+});
+
+stopBtn.addEventListener("click", () => {
+    startBtnIsActive = false;
+    clearInterval(timerId);
+});
+
+
+
 
