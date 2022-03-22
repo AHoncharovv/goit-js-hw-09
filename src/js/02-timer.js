5130e6782e79296da7fd9f076fa4320d599aa3ac
Anton Honcharov <antonhoncharovv@gmail.com>
2022-03-22 20:21:09 +0300

    makes timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,13 +2,20 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 const timerInput = document.querySelector("#datetime-picker");
 const timerField = document.querySelector("div.timer");
-const timerFieldElement = document.querySelectorAll("div.field");
-console.log(timerField.style);
 timerField.style.display = "flex";
 timerField.style.justifyContent = "space-evenly";
-// timerFieldElement.style.marginLeft = "150px";
+
+const timerDays = document.querySelector("[data-days]");
+const timerHours = document.querySelector("[data-hours]");
+const timerMinutes = document.querySelector("[data-minutes]");
+const timerSeconds = document.querySelector("[data-seconds]");
+
+
 const timerBtn = document.querySelector('button[data-start]');
-timerBtn.setAttribute('disabled', false);
+timerBtn.setAttribute('disabled', true);
+let startBtnIsActive = false;
+let closeDate = null;
+let selectedTimer = null;
 
 flatpickr(timerInput, {
     enableTime: true,
@@ -16,7 +23,69 @@ flatpickr(timerInput, {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        console.log(selectedDates[0]);
-       
+        
+
+        closeDate = Date.now();
+        if (selectedDates[0] <= closeDate) {
+            window.alert("Please choose a date in the future");
+            return;
+        };
+        timerBtn.removeAttribute('disabled');  
+        selectedTimer = selectedDates[0].getTime();
+      
     }
 });
+
+timerBtn.addEventListener('click', () => {
+    if (startBtnIsActive) {
+        return;
+    };
+    let timerToFinish = selectedTimer - Date.now(); 
+
+        timerDays.textContent = convertMs(timerToFinish).days;
+        timerHours.textContent = convertMs(timerToFinish).hours;
+        timerMinutes.textContent = convertMs(timerToFinish).minutes;
+        timerSeconds.textContent = convertMs(timerToFinish).seconds;
+    
+    let timerId = setInterval(() => {
+        timerToFinish = timerToFinish - 1000;
+        
+        timerDays.textContent = convertMs(timerToFinish).days;
+        timerHours.textContent = convertMs(timerToFinish).hours;
+        timerMinutes.textContent = convertMs(timerToFinish).minutes;
+        timerSeconds.textContent = convertMs(timerToFinish).seconds;
+
+
+        if (timerToFinish <= 1000) {
+           clearInterval(timerId);   
+        };
+    }, 1000);
+
+
+    startBtnIsActive = true; 
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+};
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+};
+        
+    
